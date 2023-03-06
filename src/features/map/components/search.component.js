36230c5efbf9107d47ee0257eee7c26fa0f09bcc
Maksym Korchak <Mkorchak1992@gmail.com>
2023-03-06 10:26:52 +0200

    Include reac-native-maps

--- src/features/map/components/search.component.js
+++ src/features/map/components/search.component.js
@@ -1,11 +1,14 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
-
 import { LocationContext } from "../../../services/location/location.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 40px;
+  width: 100%;
 `;
 
 export const Search = () => {
@@ -13,8 +16,8 @@ export const Search = () => {
   const [searchKeyword, setSearchKeyword] = useState(keyword);
 
   useEffect(() => {
-    setSearchKeyword(keyword);
-  }, [keyword]);
+    setSearchKeyword(keyword)
+  },[keyword])
 
   return (
     <SearchContainer>
